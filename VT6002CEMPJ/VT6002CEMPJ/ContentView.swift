2e7411d428cae974b25bc2e874badfd55ca76c92
itst <>
2022-12-19 15:13:50 +0800

    Created Xcode Project

---
+++ VT6002CEMPJ/VT6002CEMPJ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VT6002CEMPJ
+//
+//  Created by itst on 19/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VT6002CEMPJ/VT6002CEMPJ/VT6002CEMPJApp.swift
@@ -0,0 +1,17 @@
+//
+//  VT6002CEMPJApp.swift
+//  VT6002CEMPJ
+//
+//  Created by itst on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct VT6002CEMPJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
